3c2ea81402371cd7f89731dab92152da27de8793
Marcos Mendes <marcos.v.mendes1@gmail.com>
2024-12-16 10:45:59 -0300

    Add arquivos e redimensionamento de tela. Corrigido

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -1,15 +1,28 @@
 ﻿namespace MauiAppLogin
 {
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+      }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
-    }
+      protected override Window CreateWindow(IActivationState? activationState)
+      {
+         try
+         {
+            var window = new Window(new AppShell());
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+         }
+         catch (Exception ex)
+         {
+            throw new Exception(ex.Message);
+         }
+
+      }
+   }
 }
